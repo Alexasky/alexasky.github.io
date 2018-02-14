f4508db905a247fba6bab2cde1d21a0a884dd767
Alexasky <alex.korenkova@gmail.com>
2018-02-14 22:39:26 +0200

    commit

--- blackfriday_html/app/js/common.js
+++ blackfriday_html/app/js/common.js
@@ -3,15 +3,11 @@ $(document).ready(function(){
   var clock;
   clock = $(".clock").FlipClock({
     clockFace : "HourlyCounter",
-    autoStart : false,
+    autoStart : true,
     language : "bg",
-    callbacks :{
-      stop : function(){
-        $(".message").html("Время прошло");
-      }
-    }
+    
   });
-  var dt = "February 14 2018 20:22:48";
+  var dt = "February 15 2018 20:22:48";
   var first = new Date(dt);
   var last = Date.now();
   var remaining = first - last;
