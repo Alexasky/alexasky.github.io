046916da439d79e094574cfd32f638d5443c7658
Alexa <alex.korenkova@gmail.com>
2018-05-23 20:56:50 +0300

    commit

--- minimax/app/js/common.js
+++ minimax/app/js/common.js
@@ -2,6 +2,16 @@ $(document).ready(function(){
    $('.owl-carousel').owlCarousel({
 		 items:1,
      loop:true,
+     lazyLoad: true,
 		 animateOut: 'fadeOut'
-});
+    });
+    function heightDetect(){
+      $(".header-bg").css("height", $(window).height());
+    }
+    heightDetect();
+
+    $(window).resize(function(){
+      heightDetect();
+    });
+    $(".arrow").mPageScroll2id();
 });
